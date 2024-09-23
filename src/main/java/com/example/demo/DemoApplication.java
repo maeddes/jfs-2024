7f69eb2f84b21e7f0423ec2a23c420e226c38f29
Matthias Haeussler <maeddes@gmail.com>
2024-09-23 09:15:33 +0000

    added REST endpoint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+
+	@GetMapping("/hello")
+	public String sayHello(){
+
+		return "Hola, HSE Winter 2024";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
